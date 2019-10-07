cc2b2ef28e4c5463425ea466d547b1e2cd31e8e1
Antonio Dell'Annunziata <adannunziata@eitco.de>
2019-10-07 22:54:05 +0200

    Create main-menu component

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <li>About Me</li>
+        <li>Projects</li>
+        <li>Privat</li>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'MainMenu',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+    nav {
+        background-color: lightseagreen;
+    }
+</style>
