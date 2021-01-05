9f0df07b357fac370eb748a14fa12529684f38c0
Antonio Dell'Annunziata <Adannunziata@eitco.de>
2021-01-05 23:25:28 +0100

    Update dependencies; Add game data

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,29 +1,51 @@
 <template>
   <div>
     <h1>These are our games</h1>
-    <carousel :per-page="1">
-      <slide>
-        <iframe
-          src="https://itch.io/embed/874745"
-          height="167"
-          width="552"
-          frameborder="0"
-          ><a href="https://antonioda.itch.io/test-game"
-            >Test Game by AntonioDA</a
-          ></iframe
-        >
+    <carousel
+      :per-page="4"
+      @page-change="changeGame"
+    >
+      <slide
+        v-for="image in previewImages"
+        :key="image.name"
+      >
+        <img
+          :src="require('@/assets/' + image.name)"
+          :alt="image.alt"
+        />
       </slide>
-      <slide> Second Slide </slide>
     </carousel>
+    <iframe
+      :title="`Click to play ${currentGame.name} on itch.io!` "
+      :src="currentGame.iframeLink"
+      height="167"
+      width="552"
+    >
+    </iframe>
   </div>
 </template>
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
+import gamesData from "../data/games.json";
 
 export default {
   name: "games",
   components: { Carousel, Slide },
+  data: () => ({
+    gamesList: gamesData.list,
+    previewImages: gamesData.list.flatMap((game) => game.preview.images),
+    currentIndex: 0,
+    currentGame: gamesData.list[0],
+  }),
+  computed: {},
+  methods: {
+    changeGame(newIndex) {
+      this.currentGame = this.gamesList[newIndex];
+      this.currentIndex = newIndex;
+      console.log("new game is", this.currentGame);
+    },
+  },
 };
 </script>
 
